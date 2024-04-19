e8ba73f7210a83ec59205264402a0d67d764e8b8
vthaker165 <vthaker165@student.fuhsd.org>
2024-04-19 12:15:23 -0700

    setting up git just testing

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -20,6 +20,8 @@ public class App extends Application {
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
+        // random msg
+        
     }
 
     static void setRoot(String fxml) throws IOException {
